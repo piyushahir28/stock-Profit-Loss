2fa4795b0796a658e7cdeed96d51bbeb69f18e5d
Piyush Ahir <egpiyushahir@gmail.com>
2021-08-03 08:09:45 +0530

    feat - Image changes on diff opt

--- app.js
+++ app.js
@@ -4,31 +4,37 @@ var quantity = document.querySelector("#quantity");
 var check = document.querySelector("#check-btn");
 var output = document.querySelector(".output");
 var message = document.querySelector(".opt");
-
+var lossImage = document.querySelector(".loss");
+var profitImage = document.querySelector(".profit");
+var mainImage = document.querySelector(".hero");
 
 function clickHandler() {
-    var costPrice = CP.value;
-    var sellPrice = SP.value;
-    var qty = quantity.value;
+    var costPrice = Number(CP.value);
+    var sellPrice = Number(SP.value);
+    var qty = Number(quantity.value);
 
     if (costPrice <= 0 || sellPrice <= 0 || qty <= 0) {
         message.innerHTML = "Please Enter a valid value";
         output.style.display = "block";
     } else {
-        if (costPrice < sellPrice) {
-            var profit = ((sellPrice - costPrice) * qty).toFixed(2);
-            var profitPercent = ((profit * 100) / costPrice).toFixed(2);
-            message.innerHTML = "You gained " +
-                $ { profit }
-            " .Your total profit is ₹ " + $ { profitPercent };
+        if (costPrice > sellPrice) {
+            var loss = Math.abs(((costPrice - sellPrice) * qty)).toFixed(2);
+            var lossPercent = Math.abs((((costPrice - sellPrice) * 100) / costPrice)).toFixed(2);
+            document.documentElement.style.setProperty('--primary', 'rgb(161, 38, 38)');
+            mainImage.style.display = "none";
+            profitImage.style.display = "none";
+            message.innerHTML = "You lost " + lossPercent + " %. Your total loss is ₹ " + loss;
             output.style.display = "block";
+            lossImage.style.display = "block";
         } else {
-            var loss = ((costPrice - sellPrice) * qty).toFixed(2);
-            var lossPercent = ((loss * 100) / costPrice).toFixed(2);
-            message.innerHTML = "You lost " +
-                $ { loss }
-            " .Your total loss is ₹ " + $ { lossPercent };
+            var profit = Math.abs(((sellPrice - costPrice) * qty)).toFixed(2);
+            var profitPercent = Math.abs((((sellPrice - costPrice) * 100) / costPrice)).toFixed(2);
+            document.documentElement.style.setProperty('--primary', '#479F0B');
+            mainImage.style.display = "none";
+            lossImage.style.display = "none";
+            message.innerHTML = "You gained " + profitPercent + " %. Your total profit is ₹ " + profit;
             output.style.display = "block";
+            profitImage.style.display = "block";
         }
     }
 }
